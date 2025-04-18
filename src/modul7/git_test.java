5ddcf18483120790b92edcd1b94e166d6e096348
Badamshina.Rina <kaz_master@bk.ru>
2025-04-18 10:03:08 +0300

    commit5

--- src/modul7/git_test.java
+++ src/modul7/git_test.java
@@ -4,6 +4,6 @@ public class git_test {
 
     public static void main(String[] args) {
         System.out.println("kak dela? ");
-        System.out.println("vse horosho");
+        System.out.println("otlichno. vsem privet");
     }
 }
