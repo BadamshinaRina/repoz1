c458c725e3285827092c8ca260e16e8d0f6a9b95
Badamshina.Rina <kaz_master@bk.ru>
2025-04-18 09:43:43 +0300

    modul7.git

--- src/modul7/git_test.java
+++ src/modul7/git_test.java
@@ -3,6 +3,7 @@ package modul7;
 public class git_test {
 
     public static void main(String[] args) {
-        System.out.println("Vsem privet");
+        System.out.println("kak dela? ");
+        System.out.println("vse horosho");
     }
 }
